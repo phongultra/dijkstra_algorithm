98ba1f353fd32521d28853bbacc757fa9fe405d7
Nguyen Thanh Phong <Nguyen Thanh Phong>
2017-05-03 16:21:51 +0700

    Demo dijkstra - Phong Nguyen 1642050

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 $(function () {
+    $("#open_file_csv").on("change", function () {
+        Draw($(this).val());
+    })
     var width = 1200, height = 600,
         cRadius = 35,
         strokeWidth = 2, markerWidth = 5, markerHeight = 5,
@@ -11,165 +14,161 @@ $(function () {
         .charge(-5000)
         .linkDistance(150)//chieu dai cua duong di tuong doi
         .size([width, height]);
-    console.log(d3.select("#content"), 88888888);
-    var svg = d3.select("#content").append("svg")
-        .attr("width", width)
-        .attr("height", height);
 
-    d3.csv("/files/network.csv", function (error, data) {
 
-        var nodes = [], nodesByName = {}, links = [];
-        function addNodeByName(fullname) {
-            var name = fullname.split(',')[0];
-            if (!nodesByName[name]) {
-                var node = { "name": name, "links": [], preNode: null }
-                nodesByName[name] = node;
-                nodes.push(node);
-                console.log(1111111, node)
-                return node;
-            }
-            else
-                return nodesByName[name];
-        }
+    function Draw(filename) {
+        d3.select('#content svg').remove();
+        var svg = d3.select("#content").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+        d3.csv("/files/" + filename + ".csv", function (error, data) {
 
-        data.forEach(function (d) {
-            for (k in d) {
-                if (d.hasOwnProperty(k) && k != "nodes" && d[k] < 750) {
-                    links.push({ "source": addNodeByName(d["nodes"]), "target": addNodeByName(k), "value": parseInt(d[k]) /*parseInt(Math.random()*d[k]*5+1)*/, 'id': addNodeByName(d["nodes"]).name + addNodeByName(k).name })
+            var nodes = [], nodesByName = {}, links = [];
+            function addNodeByName(fullname) {
+                var name = fullname.split(',')[0];
+                if (!nodesByName[name]) {
+                    var node = { "name": name, "links": [], preNode: null }
+                    nodesByName[name] = node;
+                    nodes.push(node);
+                    return node;
                 }
+                else
+                    return nodesByName[name];
             }
-        });
 
-        force.nodes(nodes).links(links).start();
-        /*Set Marker arrow in the Link*/
-        svg.append("svg:defs").selectAll("marker")
-            .data(["arrow"])
-            .enter().append("svg:marker")
-            .attr("id", String)
-            .attr("viewBox", "0 -5 10 10")/*Hinh tam giac*/
-            .attr("refX", refX)/*Toa do x*/
-            .attr("refY", refY)/*Toa do y*/
-            .attr("markerWidth", markerWidth)
-            .attr("markerHeight", markerHeight)
-            .attr("orient", "auto")
-            .attr("class", "marker")
-            .append("svg:path")
-            .attr("d", "M0,-5L10,0L0,5");
+            data.forEach(function (d) {
+                for (k in d) {
+                    if (d.hasOwnProperty(k) && k != "nodes" && d[k] < 750) {
+                        links.push({ "source": addNodeByName(d["nodes"]), "target": addNodeByName(k), "value": parseInt(d[k]) /*parseInt(Math.random()*d[k]*5+1)*/, 'id': addNodeByName(d["nodes"]).name + addNodeByName(k).name })
+                    }
+                }
+            });
 
-        var glink = svg.append('g').selectAll(".link")
-            .data(links).enter()
+            force.nodes(nodes).links(links).start();
+            /*Set Marker arrow in the Link*/
+            svg.append("svg:defs").selectAll("marker")
+                .data(["arrow"])
+                .enter().append("svg:marker")
+                .attr("id", String)
+                .attr("viewBox", "0 -5 10 10")/*Hinh tam giac*/
+                .attr("refX", refX)/*Toa do x*/
+                .attr("refY", refY)/*Toa do y*/
+                .attr("markerWidth", markerWidth)
+                .attr("markerHeight", markerHeight)
+                .attr("orient", "auto")
+                .attr("class", "marker")
+                .append("svg:path")
+                .attr("d", "M0,-5L10,0L0,5");
 
-        var link = glink.append("line")
-            .attr("class", function (d) {
-                return "link " + d.id
-            })
-            .style("stroke-width", strokeWidth)
-            .attr("marker-end", function (d) {
-                return "url(#arrow)";
+            var glink = svg.append('g').selectAll(".link")
+                .data(links).enter()
+
+            var link = glink.append("line")
+                .attr("class", function (d) {
+                    return "link " + d.id
+                })
+                .style("stroke-width", strokeWidth)
+                .attr("marker-end", function (d) {
+                    return "url(#arrow)";
+                });
+
+
+            var linktext = glink.append('text')
+                .attr("class", function (d) {
+                    return "linktext " + d.id
+                })
+                .attr('text-anchor', 'middle')
+                .style('opacity', 1)
+                .text(function (d) {
+                    return d.value
+                })
+
+            var gnode = svg.append('g').selectAll(".node")
+                .data(nodes).enter()
+
+            var node = gnode.append("circle")
+                .attr("class", function (d) {
+                    return 'node ' + d.name
+                })
+                .attr("r", cRadius)
+                .call(force.drag);
+
+            var nodetext = gnode.append('text')
+                .attr('text-anchor', 'middle')
+                .attr('dy', '0.35em')
+                .attr("class", function (d) {
+                    return "nodetext " + d.name
+                })
+                .text(function (d) {
+                    return d.name
+                })
+
+
+            link.each(function (d) {
+                d.source.links.push(d);
+                d.selection = d3.select(this);
+            });
+
+            node.each(function (d) {
+                d.selection = d3.select(this);
+            });
+
+            force.on("tick", function () {
+                link.attr("x1", function (d) {
+                    return d.source.x;
+                })
+                    .attr("y1", function (d) {
+                        return d.source.y;
+                    })
+                    .attr("x2", function (d) {
+                        return d.target.x;
+                    })
+                    .attr("y2", function (d) {
+                        return d.target.y;
+                    });
+
+                linktext.attr("x", function (d) {
+                    return (d.source.x + d.target.x) / 2;
+                })
+                    .attr("y", function (d) {
+                        return (d.source.y + d.target.y) / 2;
+                    })
+
+                nodetext.attr("x", function (d) {
+                    return d.x;
+                })
+                    .attr("y", function (d) {
+                        return d.y;
+                    })
+
+                node.attr("cx", function (d) {
+                    return d.x;
+                })
+                    .attr("cy", function (d) {
+                        return d.y;
+                    });
             });
 
 
-        var linktext = glink.append('text')
-            .attr("class", function (d) {
-                return "linktext " + d.id
-            })
-            .attr('text-anchor', 'middle')
-            .style('opacity', 1)
-            .text(function (d) {
-                return d.value
-            })
+            node.on("mouseover", function (d) {
+                d3.select(this).attr('r', cRadius * 110 / 100);
+                //Show Shortest path from Start to This d
+                dijkstra.colorShortestPath(d);
+            });
 
-        var gnode = svg.append('g').selectAll(".node")
-            .data(nodes).enter()
+            node.on("mouseout", function (d) {
+                d3.select(this).attr('r', cRadius);
+                d3.selectAll(".link").style('stroke', "#000000");
+            });
 
-        var node = gnode.append("circle")
-            .attr("class", function (d) {
-                return 'node ' + d.name
-            })
-            .attr("r", cRadius)
-            .call(force.drag);
+            var dijkstra = d3.dijkstra()
+                .nodes(nodes)
+                .edges(links);
 
-        var nodetext = gnode.append('text')
-            .attr('text-anchor', 'middle')
-            .attr('dy', '0.35em')
-            .attr("class", function (d) {
-                return "nodetext " + d.name
-            })
-            .text(function (d) {
-                return d.name
-            })
-
-
-        link.each(function (d) {
-            d.source.links.push(d);
-            d.selection = d3.select(this);
+            node.on("click", dijkstra.runAllShortestPathFromStart);
         });
-
-        node.each(function (d) {
-            d.selection = d3.select(this);
-        });
-
-        force.on("tick", function () {
-            link.attr("x1", function (d) {
-                return d.source.x;
-            })
-                .attr("y1", function (d) {
-                    return d.source.y;
-                })
-                .attr("x2", function (d) {
-                    return d.target.x;
-                })
-                .attr("y2", function (d) {
-                    return d.target.y;
-                });
-
-            linktext.attr("x", function (d) {
-                return (d.source.x + d.target.x) / 2;
-            })
-                .attr("y", function (d) {
-                    return (d.source.y + d.target.y) / 2;
-                })
-
-            nodetext.attr("x", function (d) {
-                return d.x;
-            })
-                .attr("y", function (d) {
-                    return d.y;
-                })
-
-            node.attr("cx", function (d) {
-                return d.x;
-            })
-                .attr("cy", function (d) {
-                    return d.y;
-                });
-        });
-
-
-        node.on("mouseover", function (d) {
-            d3.select(this).attr('r', cRadius * 110 / 100);
-            //Show Shortest path from Start to This d
-            dijkstra.colorShortestPath(d);
-        });
-
-
-
-
-        node.on("mouseout", function (d) {
-            d3.select(this).attr('r', cRadius);
-            d3.selectAll(".link").style('stroke', "#000000");
-        });
-
-        var dijkstra = d3.dijkstra()
-            .nodes(nodes)
-            .edges(links);
-
-        node.on("click", dijkstra.runAllShortestPathFromStart);
-        // document.getElementById("test").addEventListener("click", function (e) {
-        //     e.preventDefault();
-        //     dijkstra.runAllShortestPathFromStartToGoal(nodes[0], nodes[7]);
-        // });
-    });
+    }
+    Draw($("#open_file_csv").val())
 
     d3.dijkstra = function () {
         var dijkstra = {}, nodes, edges, source, go, dispatch = d3.dispatch("start", "tick", "step", "end");
@@ -369,4 +368,5 @@ $(function () {
 
         return d3.rebind(dijkstra, dispatch, "on", "end", "start", "tick");
     };
+
 })
